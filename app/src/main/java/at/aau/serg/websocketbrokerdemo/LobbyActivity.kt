af1150ef287dbe8e07c6f6eaf43a4f58486e1ed3
Vanesa Markovic <deine.uni.email@aau.at>
2026-05-03 21:32:01 +0200

    feat(lobby): navigate to main screen on leave

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/LobbyActivity.kt
@@ -68,6 +68,10 @@ class LobbyActivity : ComponentActivity() {
 
         btnLeave.setOnClickListener {
             MyStomp.instance.leaveLobby()
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
         LobbyHandler.onPlayerRemoved = {
             runOnUiThread {
