23718a6b9d0f68bb3ff5b9cc186d181b78f48a31
Lusine Babelyan <lubabelyan@edu.aau.at>
2026-05-11 22:07:12 +0200

    feat: check if it is the users turn

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/GameActivity.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/GameActivity.kt
@@ -9,4 +9,12 @@ class GameActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
     }
+
+    private fun isMyTurn(): Boolean {
+        val players = ClientState.players
+        if (players.isEmpty() || ClientState.currentPlayerIndex >= players.size) return false
+        return players[ClientState.currentPlayerIndex].playerId == ClientState.playerId
+    }
+    private fun setupGameHandlers() {
+    }
 }
